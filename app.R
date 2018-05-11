b71e60634076c6c1f0f60ad63ed91fe37b43e6ff
jcvdav <juancarlos.villader@gmail.com>
2018-05-11 11:21:47 -0700

    Added download and bookmarking button to box on sidebar

--- app.R
+++ app.R
@@ -130,8 +130,16 @@ ui <- dashboardPage(title = "Costeo de Reservas",
                                       outputId = "totalMXP",
                                       width = 12)),
                                   fluidRow(
-                                    downloadButton(outputId = "download_total",
-                                                   label = "Descargar presupuesto")
+                                    box(title = "Herramientas",
+                                        status = "primary",
+                                        background = "blue",
+                                        width = 12,
+                                        collapsible = T,
+                                        collapsed = T,
+                                        downloadButton(outputId = "download_total",
+                                                       label = "Descargar presupuesto"),
+                                        bookmarkButton(title = "Compartir", label = "Compartir")
+                                    )
                                   )
                                   
                       )
@@ -139,7 +147,22 @@ ui <- dashboardPage(title = "Costeo de Reservas",
                     body = dashboardBody(
                       tabItems(
                         tabItem(tabName = "inicio",
-                                "Informacion de la app aqui"),
+                                box(title = h1("Costeo de Reservas Marinas"),
+                                    status = "primary",
+                                    p("Las reservas marinas completamente protegidas son áreas del océano restringidas a cualquier actividad extractiva, incluyendo la pesca. Las reservas marinas exitosas crean condiciones en las que las poblaciones de especies previamente capturadas se pueden recuperar y restaurar el equilibrio trófico en el ecosistema. La recuperación de la biomasa pesquera dentro de la reserva puede causar efectos colaterales en las zonas de pesca adyacentes, tanto desde el traslado de especímenes adultos, como en la exportación de larvas. Este efecto de desbordamiento puede ayudar a los usuarios a compensar algunos de los costos de oportunidad al ceder las zonas de pesca. La reserva marina funciona como una cuenta bancaria, la cual se repobla con el interés al capital con el desbordamiento."),
+                                    p(a("COBI", href = "www.cobi.org.mx", target = "_blank"),"ha trabajado durante 19 años para establecer, evaluar y mantener las reservas marinas en colaboración con las comunidades pesqueras de México. Nuestro modelo de reservas marinas consiste de cuatro fases:"),
+                                    tags$ul(
+                                      tags$li(tags$b("1) Implementación - "),"procesos inclusivos en el que las partes interesadas participen en el proceso de diseño, definición de objetivos y la selección del sitio"),
+                                      tags$li(tags$b("2) Monitoreo - "),"después de que la reserva se ha creado, miembros de la comunidad están capacitados para recopilar datos para evaluar la reserva"),
+                                      tags$li(tags$b("3) Operación – "),"acciones relacionadas a la vigilancia comunitaria, señalización y comunicación de resultados"),
+                                      tags$li(tags$b("4) Renovación – "),"manejo adaptativo basado en los datos recogidos por la comunidad para garantizar el funcionamiento eficaz de la reserva.")
+                                    ),
+                                    p("El número de iniciativas para establecer redes de reservas marinas está en aumento, y la gran mayoría de los esfuerzos se realizan con fondos filantrópicos. Sin embargo, al iniciar el proceso para establecer reservas marinas es común que los costos proyectados al futuro no estén claramente definidos. Esto puede afectar la sustentabilidad de la reserva marina al largo plazo, sobre todo si el costo de mantenerla y operarla es mayor al beneficio que puede proporcionar a la comunidad."),
+                                    p("Este calculador de costos contempla todos los pasos necesarios para establecer una reserva marina utilizado el modelo COBI, con el objetivo de ayudar a comunidades, organizaciones de la sociedad civil y tomadores de decisiones de planear sus inversiones con mayor claridad y transparencia."),
+                                    p("Para cualquier pregunta o comentario sobre este producto escribe al correo rema@cobi.org.mx Tus observaciones nos ayudarán a mejorar nuestras herramientas."),
+                                    p("© COBI 2018")
+                                )
+                        ),
                         tabItem(tabName = "implementacion",
                                 fluidRow(
                                   box(title = "Definición de objetivos",
