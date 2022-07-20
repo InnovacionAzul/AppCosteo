f3abf996c543cc4c1bb7d572b876b5b0edf32513
Kat Millage <kmillage@ucsb.edu>
2022-07-20 10:56:01 -0700

    automated ui generation, close #26

--- app.R
+++ app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(shinydashboard)
-library(xlsx)
+#library(xlsx)
 library(magrittr)
 library(tidyr)
 library(dplyr)
@@ -20,42 +20,31 @@ library(shinyBS)
 library(purrr)
 library(readxl)
 
+### Read in data 
 rema_data <- readxl::read_xlsx("www/defaults_rema_fip.xlsx",
                                sheet = 1,
                                na = c("", "N/A")) %>%
   janitor::clean_names() %>%
-  arrange(fase, subfase, concepto, actividad) %>%
+  arrange(fase, subfase_orden, actividad_orden) %>%
   mutate(valor_unitario = ifelse(is.na(valor_unitario), 0, valor_unitario),
-         estimacion_de_unidades_requeridas = ifelse(is.na(estimacion_de_unidades_requeridas), 0, estimacion_de_unidades_requeridas)) %>%
+         estimacion_de_unidades_requeridas = ifelse(is.na(estimacion_de_unidades_requeridas), 0, estimacion_de_unidades_requeridas),
+         unidades = ifelse(is.na(unidades), "$/unidad", unidades)) %>%
   mutate(section = "REMA")
 
 fip_data <- readxl::read_xlsx("www/defaults_rema_fip.xlsx",
                                sheet = 2,
                                na = c("", "N/A")) %>%
   janitor::clean_names() %>%
-  arrange(fase, subfase, concepto, actividad) %>%
+  arrange(fase, subfase_orden, actividad_orden) %>%
   mutate(valor_unitario = ifelse(is.na(valor_unitario), 0, valor_unitario),
-         estimacion_de_unidades_requeridas = ifelse(is.na(estimacion_de_unidades_requeridas), 0, estimacion_de_unidades_requeridas)) %>%
+         estimacion_de_unidades_requeridas = ifelse(is.na(estimacion_de_unidades_requeridas), 0, estimacion_de_unidades_requeridas),
+         unidades = ifelse(is.na(unidades), "$/unidad", unidades)) %>%
   mutate(section = "FIP")
 
 cost_data <- rema_data %>%
   bind_rows(fip_data)
 
-# cost_data <- read.csv("www/cost_data.csv",
-#                       stringsAsFactors = F,
-#                       strip.white = T,
-#                       na.strings = "N/A") %>% 
-#   janitor::clean_names() %>% 
-#   arrange(fase, tipo_de_inversion, periodicidad, concepto, subactividad, elemento) %>% 
-#   mutate(valor_unitario_usd = ifelse(
-#     is.na(valor_unitario_usd),
-#     0,
-#     valor_unitario_usd),
-#     estimacion_de_unidades_requeridas = ifelse(
-#       is.na(estimacion_de_unidades_requeridas),
-#       0,
-#       estimacion_de_unidades_requeridas))
-
+### Cost and quantity header for every activity
 boxHeaderUI <- function(){
   tagList(
     fluidRow(
@@ -66,6 +55,7 @@ boxHeaderUI <- function(){
   )
 }
 
+### Create cost and quantity input row for every activity
 CostUnitUI <- function(titleId, pairId, costLabel, unitLabel){
   
   # Define inputId labels for cost and units
@@ -125,14 +115,20 @@ makeUnit <- function(activity, data_subphase){
 activityWrapper <- function(subphase, number, data_fase){
   
   data_subphase <- data_fase %>%
-    dplyr::filter(subfase == subphase)
+    dplyr::filter(subfase == subphase) %>%
+    arrange(actividad_orden)
   
   activities <- unique(data_subphase$actividad)
   
   tagList(
+    tags$div(style = "border: 1px solid lightgray; margin: 15px; padding: 15px 15px 0px 0px; border-radius: 5px;",
     fluidRow(
       column(3,
-             valueBox(value = number, subtitle = subphase, icon = NULL, color = "aqua", width = 12,
+             valueBox(value = number, 
+                      subtitle = subphase, 
+                      icon = NULL, 
+                      color = "blue", 
+                      width = 12,
                       href = NULL)
       ),
       column(9,
@@ -142,21 +138,30 @@ activityWrapper <- function(subphase, number, data_fase){
                    data_subphase = data_subphase)
              )
       )
-    ),
-    tags$hr()
+    )
+    )
   )
   
 }
 
 ### Filters the data by section and phase and finds all subphases to iterate over 
-subphaseWrapper <- function(data, phase, section){
+subphaseWrapper <- function(data, phase, section, subphases_to_include = NULL){
 
   data_fase <- filter(data,
                       fase == phase,
-                      section == section)
+                      section == section) %>%
+    arrange(subfase_orden)
   
-  subfases <- unique(data_fase$subfase)
-  numbers <- seq(1:length(subfases))
+  if(phase == "Implementación" & section == "FIP"){
+    subfases <- subphases_to_include
+    numbers <- seq(1:length(subfases))
+
+  }else{
+    subfases <- unique(data_fase$subfase)
+    numbers <- seq(1:length(subfases))
+  }
+
+  #req(length(subfases) >= 1)
   
   tagList(
     map2(.x = subfases,
@@ -164,6 +169,59 @@ subphaseWrapper <- function(data, phase, section){
         .f = activityWrapper,
         data_fase = data_fase)
   )
+}
+
+### Duration
+makePhaseDuration <- function(phase, section){
+  
+  # Define inputId labels for cost and units
+  durationId <- paste0("d_", tolower(substr(x = phase, start = 1, stop = 3)), "_", tolower(section))
+
+  if(phase == "Implementación" & section == "FIP"){
+    tagList(
+      tags$div(style = "margin: 15px; padding: 15px 15px 15px 0px; border-radius: 5px;",
+               fluidRow(
+                 column(width = 6,
+                        valueBox(value = NULL,
+                                 subtitle = numericInput(durationId,
+                                                         label = "Duración de la fase (años)",
+                                                         value = 1,
+                                                         min = 1,
+                                                         max = 20,
+                                                         width = "100%"),
+                                 width = 12,
+                                 color = "aqua")
+                 ),
+                 valueBox(value = NULL,
+                          subtitle = selectInput("choices_imp_fip",
+                                                 label = "Actividades de implementación FIP para incluir",
+                                                 choices = unique(fip_data$subfase[fip_data$fase == "Implementación"]),
+                                                 multiple = T,
+                                                 width = "100%"),
+                          width = 6,
+                          color = "aqua")
+               )
+      )
+    )
+  }else{
+    tagList(
+      tags$div(style = "margin: 15px; padding: 15px 15px 15px 0px; border-radius: 5px;",
+               fluidRow(
+                 column(width = 6,
+                        valueBox(value = NULL,
+                                 subtitle = numericInput(durationId,
+                                                         label = "Length of phase (años)",
+                                                         value = 1,
+                                                         min = 1,
+                                                         max = 20,
+                                                         width = "100%"),
+                                 width = 12,
+                                 color = "aqua")
+                 )
+               )
+      )
+    )
+  }
 }
 
 # Define UI for application that draws a histogram
@@ -182,21 +240,12 @@ ui <- dashboardPage(title = "Costeo de Intervenciones",
                                     text = "Costeo de Reservas",
                                     tabName = "rema",
                                     icon = icon("dollar-sign")#,
-                                    # menuSubItem(text = "Implementación",
-                                                # tabName = "implementacion"),
-                                    # menuSubItem(text = "Monitoreo",
-                                                # tabName = "monitoreo"),
-                                    # menuSubItem(text = "Operación",
-                                                # tabName = "operacion"),
-                                    # menuSubItem(text = "Renovación",
-                                                # tabName = "renovacion")
                                     ),
                                   menuItem(
                                     text = "Costeo de FIP",
                                     tabName = "fip",
                                     icon = icon("dollar-sign")
                                   ),
-                                  
                                   menuItem(
                                     text = "Presupuesto",
                                     tabName = "presupuesto",
@@ -204,7 +253,11 @@ ui <- dashboardPage(title = "Costeo de Intervenciones",
                                   ),
                                   fluidRow(
                                     infoBoxOutput(
-                                      outputId = "totalMXP",
+                                      outputId = "REMAtotalUSD",
+                                      width = 12)),
+                                  fluidRow(
+                                    infoBoxOutput(
+                                      outputId = "FIPtotalUSD",
                                       width = 12)),
                                   fluidRow(
                                     box(title = "Herramientas",
